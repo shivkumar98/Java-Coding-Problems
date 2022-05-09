2306866900f06aa76b6dc6a5eac38f4fe16ea447
shivkumar98 <shiv_kumar.98@outlook.com>
2022-05-09 10:56:57 +0100

    Finished problems 1 and 2 of chapter 1: Strings

---
+++ src/Chapter_1_Strings/Question1_CountDuplicateCharacters_Solution.java
@@ -0,0 +1,32 @@
+package Chapter_1_Strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Question1_CountDuplicateCharacters_Solution {
+
+	/*
+	 * Counting duplicate characters: Write a program that counts duplicate
+characters from a given string.
+	 */
+	
+	//we can use a map to store the key, value pairs
+	
+	static String CountDuplicateCharcters(String str) {
+		Map<Character, Integer> characterCount = new HashMap<>();
+		
+		for (int i =0; i<str.length();i++) {
+			char c = str.charAt(i);
+			
+			characterCount.compute(c, (k,v)-> (v==null)?1:++v);
+		}
+		
+		return characterCount.toString();
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(CountDuplicateCharcters("Hello"));
+	}
+}
+
---
+++ src/Chapter_1_Strings/Question_2_FirstNonRepeatedCharacter.java
@@ -0,0 +1,34 @@
+package Chapter_1_Strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Question_2_FirstNonRepeatedCharacter {
+
+	
+	/*
+	 * Write a program that returns the
+first non-repeated character from a given string.
+	 */
+	
+	static Character firstNonRepeatedChar(String str) {
+		
+		Map<Character,Integer> charCount = new HashMap<>();
+		
+		for (int i =0; i<str.length();i++) {
+			charCount.compute(str.charAt(i), (k,v) -> (v == null)?1:0); // set to 0 if not null
+		}
+		
+		for (int i=0; i<str.length();i++) {
+			if (charCount.get(str.charAt(i)) == 1){
+				return str.charAt(i);
+			} 
+		}
+		return '!';
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(firstNonRepeatedChar("hhll")); // !
+		System.out.println(firstNonRepeatedChar("hello")); // h
+	}
+}
