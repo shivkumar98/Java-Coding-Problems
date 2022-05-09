fa3944f780dd79df30b9ea53f70dea69110a44d4
shivkumar98 <shiv_kumar.98@outlook.com>
2022-05-09 11:17:47 +0100

    Finished my attempt of Question 3, chapter 1

---
+++ src/Chapter_1_Strings/Question_3_ReverseLettersAndWords.java
@@ -0,0 +1,40 @@
+package Chapter_1_Strings;
+
+public class Question_3_ReverseLettersAndWords {
+	
+	/*
+	 *  Write a program that reverses the letters of
+each word and a program that reverses the letters of each word and the
+words themselves.
+	 */
+	
+	static String reverseLettersOfWords(String str) {
+		String[] splitString = str.split(" ");
+		String reversedString = "";
+		
+		for (int i = 0; i<splitString.length-1;i++ ) {
+			reversedString += reverseWord(splitString[i]) + " ";
+		}
+			reversedString+=reverseWord(splitString[splitString.length-1]);
+		
+		return reversedString;
+		
+	}
+	
+	private static String reverseWord(String string) {
+		return new StringBuilder(string).reverse().toString();
+	}
+	
+	static String reverseLettersAndWords(String str) {
+		
+		return new StringBuilder(str).reverse().toString();
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(reverseLettersOfWords("hello world"));
+		
+		System.out.println(reverseLettersAndWords("hello world"));
+	}
+
+}
