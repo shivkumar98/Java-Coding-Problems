ed0bb94d6a561a35bb96b9e3ccf116edf8505eb8
shivkumar98 <shiv_kumar.98@outlook.com>
2022-05-10 11:30:56 +0100

    Finished Question 7 of Chapter 1

---
+++ src/Chapter_1_Strings/Question_7_ConvertStringToIntLongFloat.java
@@ -0,0 +1,59 @@
+package Chapter_1_Strings;
+
+import java.text.NumberFormat;
+
+public class Question_7_ConvertStringToIntLongFloat {
+
+	/*
+	 * 7. Converting String into int, long, float, or double: Write a program
+that converts the given String object (representing a number) into int,
+long, float, or double
+	 */
+	
+	static Number convertToNumber(String str) {
+		
+		//assuming positive numbers
+		
+		boolean isInteger = true;
+		// checking if integer
+		for (int i = 0; i <str.length(); i++) {
+			;
+			if (!Character.isDigit(str.charAt(i))) {
+				isInteger=false;
+				break;
+			}
+		}
+		
+		try {
+			int x = Integer.valueOf(str);
+			return x;
+		} catch (NumberFormatException e) {
+			try {
+				long x = Long.valueOf(str);
+				return x;
+			} catch (NumberFormatException ef) {
+				try {
+					double x = Double.valueOf(str);
+					return x;
+				} catch (NumberFormatException egg) {
+					try {
+						float x = Float.valueOf(str);
+						return x;
+					} catch (Exception eggs) {
+						return 0;
+					}
+				}
+				
+			}
+		}
+		
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(convertToNumber("1.223f"));
+		System.out.println(Integer.MAX_VALUE);
+	}
+}
