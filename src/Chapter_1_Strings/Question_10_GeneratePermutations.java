a884b6efa8087af5443fa39b06b407751db36eb3
shivkumar98 <shiv_kumar.98@outlook.com>
2022-05-11 17:50:47 +0100

    Started generation of permutations question

---
+++ src/Chapter_1_Strings/Question_10_GeneratePermutations.java
@@ -0,0 +1,29 @@
+package Chapter_1_Strings;
+
+import java.sql.Array;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Question_10_GeneratePermutations {
+	
+	/*
+	 * 10. Generating all permutations: Write a program that generates all of the
+permutations of a given string.
+	 */
+
+	static List<String> getAllPermutations(String str){
+		List<String> list = new ArrayList<String>();
+		list.add(str);
+		
+		for (int i =0; i<str.length();i++)
+		
+		
+		return list;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println());
+	}
+}
