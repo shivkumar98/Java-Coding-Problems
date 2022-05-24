9a135e7ae7672447a309305c9d9468a537918fcf
shivkumar98 <shiv_kumar.98@outlook.com>
2022-05-24 17:12:47 +0100

    Renamed files

---
+++ src/Chapter_1_Strings/Q16_CheckStringForSubString.java
@@ -0,0 +1,24 @@
+package Chapter_1_Strings;
+
+/*
+ * Write a program that checks
+whether the given string contains the given substring.
+ */
+
+public class Q16_CheckStringForSubString {
+	
+	public static boolean containsSubstring(String string, String substring) {
+		StringBuilder sb = new StringBuilder(string);
+		
+		if (sb.indexOf(substring) == -1) {
+			return false;
+		} return true;
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(containsSubstring("hello", "ello")); // true
+		System.out.println(containsSubstring("hello", "ello dere")); // false
+	}
+	
+}
