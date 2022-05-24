664d60d78919195333bd9abb4d132e5336cc9856
shivkumar98 <shiv_kumar.98@outlook.com>
2022-05-24 14:27:52 +0100

    Finished Q14 - Finding character with most occurences

---
+++ src/Chapter_1_Strings/Question_13_RemoveGivenCharacters.java
@@ -0,0 +1,20 @@
+package Chapter_1_Strings;
+
+public class Question_13_RemoveGivenCharacters {
+	
+	/*
+	 * Write a program that removes the given
+		character from the given string
+	 */
+	
+	public static String removeCharacter(char c, String str) {
+		
+		return str.replace(String.valueOf(c), "");
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(removeCharacter('x', "tex mex")); // te me
+	}
+
+}
---
+++ src/Chapter_1_Strings/Question_14_FindCharWithMostAppearances.java
@@ -0,0 +1,37 @@
+package Chapter_1_Strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Question_14_FindCharWithMostAppearances {
+
+	
+	/*
+	 *  Write a program that
+finds the character with the most appearances in the given string.
+	 */
+	
+	public static char charWithMostOccurences(String str) {
+		StringBuilder sb= new StringBuilder(str);
+		Map<Character, Integer> charCount = new HashMap<>();
+		
+		char[] charArray = str.toCharArray();
+		for (char c: charArray) {
+			charCount.compute(c, (k,v)-> (v==null)?1:++v);
+		}
+		
+		int max = 0;
+		char highestOccurence = ' ';
+		for (char c:charCount.keySet()) {
+			if (charCount.get(c) > max) {
+				max = charCount.get(c);
+				highestOccurence=c;
+			}
+		}
+		return highestOccurence;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(charWithMostOccurences("aaaaaabbbbbbbbbbcccccccccc")); //b
+	}
+}
