d7b7093261d70ba9938c6b83cf5236e65453b83d
shivkumar98 <shiv_kumar.98@outlook.com>
2022-07-26 10:44:42 +0100

    Renamed files

---
+++ src/Chapter_1_Strings/Q04_CheckingIfStringContainsDigits.java
@@ -0,0 +1,25 @@
+package Chapter_1_Strings;
+
+public class Q04_CheckingIfStringContainsDigits {
+
+	/*Checking whether a string contains only digits: Write a program that
+checks whether the given string contains only digits
+	 */
+	
+	static boolean containsOnlyDigits(String str) {
+		String[] splitString = str.split("");
+		for (int i =0; i < splitString.length; i ++) {
+			if (!splitString[i].matches("[0-9]+")){
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(containsOnlyDigits("1e2323")); // false
+		System.out.println(containsOnlyDigits("00000")); // true
+	}
+	
+	
+}
