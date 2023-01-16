00c7957d60e93e9fa5930ee37c5dce301b2d6e4e
shivkumar98 <58525809+shivkumar98@users.noreply.github.com>
2023-01-16 10:23:08 +0000

    Fixed typo
    Accidentally name Q19_solution as Q20_solution

--- src/chapter_1_strings/attempt2/Q19_solution.java
+++ src/chapter_1_strings/attempt2/Q19_solution.java
@@ -1,6 +1,6 @@
 package chapter_1_strings.attempt2;
 
-public class Q20_solution {
+public class Q19_solution {
 	 // in Java 13 we can use triple quotations
 	public static void main(String[] args) {
 		/// in JDK 8, we can use a Line separator
