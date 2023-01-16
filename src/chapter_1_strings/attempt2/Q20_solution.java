ffb96c53affd578ae12b6b596be8139c0cfb5f7b
shivkumar98 <58525809+shivkumar98@users.noreply.github.com>
2023-01-16 10:22:08 +0000

    Q20: multiline text

---
+++ src/chapter_1_strings/attempt2/Q20_solution.java
@@ -0,0 +1,12 @@
+package chapter_1_strings.attempt2;
+
+public class Q20_solution {
+	 // in Java 13 we can use triple quotations
+	public static void main(String[] args) {
+		/// in JDK 8, we can use a Line separator
+		// we can use the String's join() method
+		String multilineText = String.join(System.lineSeparator(),
+				"this is line 1", "my name is shiv", "final line");
+		System.out.println(multilineText);
+	}
+}
